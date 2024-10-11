75019ceeee60bcf351a0c422c427b2791b569661
ranjan9678 <ranjan600345@gmail.com>
2024-10-11 19:23:05 +0530

    1st time commit

---
+++ tic toc toe/app.js
@@ -0,0 +1,18 @@
+let boxes= document.querySelectorAll(".box");
+let restBtn=document.querySelector("#reset-btn");
+
+const winPatterns=[
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [3,4,5],
+  [6,7,8],
+];
+boxes.forEach((box)=>{
+  box.addEventListener("click", () =>{
+    console.log("box clicked");
+  })
+})
